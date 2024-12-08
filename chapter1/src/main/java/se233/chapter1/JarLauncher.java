ee0770572050a56d89d407a486d3fc0e63c8dcc0
Ratthasas <ratthasas_sing@cmu.ac.th>
2024-12-08 18:55:16 +0700

    Delete chapter1 directory

--- chapter1/src/main/java/se233/chapter1/JarLauncher.java
+++
@@ -1,7 +0,0 @@
-package se233.chapter1;
-
-public class JarLauncher {
-    public static void main(String[] args) {
-        Launcher.main(args);
-    }
-}
--- chapter1/src/main/java/se233/chapter1/model/DamageType.java
+++
@@ -1,7 +0,0 @@
-package se233.chapter1.model;
-
-public enum DamageType {
-    physical,
-    magical,
-    mixed
-}
